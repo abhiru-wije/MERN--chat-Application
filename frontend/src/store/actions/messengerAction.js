4906787dad7440012303c127ea476cf290d795a8
abhiru-wije <abdwije19972219@gmail.com>
2023-02-01 02:03:51 +0530

    messengerAction updated

--- frontend/src/store/actions/messengerAction.js
+++ frontend/src/store/actions/messengerAction.js
@@ -4,7 +4,13 @@ import axios from "axios"
 export const getFriends = () => async(dispatch) => {
     try{
         const response = await axios.get('/api/messenger/get-friends');
-        console.log(response.data)
+        dispatch({
+            type: FRIEND_GET_SUCCESS,
+            payload: {
+                friends : response.data.friends
+            }
+        })
+        
     }catch(error){
         console.log(error.response.data)
     }
