d30be8ac945ff5932a15d3553ceb48f94a6b17d3
abhiru-wije <abdwije19972219@gmail.com>
2023-02-01 11:37:58 +0530

    message get and display started

--- backend/controller/messengerController.js
+++ backend/controller/messengerController.js
@@ -1,4 +1,5 @@
 const User = require('../models/authModel')
+const messageModel = require('../models/messageModel');
 
 module.exports.getFriends = async (req, res)=> {
     const myId = req.myId;
@@ -16,7 +17,33 @@ module.exports.getFriends = async (req, res)=> {
     }
 }
 module.exports.messageUploadDB = async (req, res) => {
+
+    const {
+        senderName,
+        receiverId,
+        message
+    } = req.body
     const senderId = req.myId
-    console.log(req.body);
-    console.log(senderId);
+    
+    try{
+        const insertMessage = await messageModel.create({
+            senderId: senderId,
+            senderName: senderName,
+            receiverId: receiverId,
+            message: {
+                text: message,
+                image: ''
+            }
+        })
+        res.status(201).json({
+            success: true,
+            message: insertMessage
+        })
+    }catch (err) {
+        res.status(500).json({
+            error:{
+                errorMessage: 'Internal Server Error'
+            }
+        })
+    }
 }
