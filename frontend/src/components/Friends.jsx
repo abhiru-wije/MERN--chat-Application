03c12ee127dddf8dbdfd53bf48fef4427a0a5b34
abhiru-wije <abdwije19972219@gmail.com>
2023-02-04 20:50:30 +0530

    show last message started

--- frontend/src/components/Friends.jsx
+++ frontend/src/components/Friends.jsx
@@ -1,17 +1,23 @@
 /* eslint-disable no-template-curly-in-string */
 import React from 'react'
 
-const Friends = ({friend}) => {
+const Friends = (props) => {
+  const {fndInfo, msgInfo} = props.friend;
   return (
     <div className="friend">
         <div className="friend-image">
             <div className="image">
-            <img src={`./image/${friend.fndInfo.image}`} alt="" />
+            <img src={`./image/${fndInfo.image}`} alt="" />
             </div>
         </div>
         <div className="friend-name-seen">
             <div className="friend-name">
-                <h4>{friend.fndInfo.userName}</h4>
+                <h4>{fndInfo.userName}</h4>
+                <div className="msg-time">
+                  {
+                    msgInfo && msgInfo.senderId
+                  }
+                </div>
             </div>
         </div>
     </div>
